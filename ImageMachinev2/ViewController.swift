4a3bb82c70195263fce870f2f68bfba7bfe8b6f1
ilyasa Azmi <theazmi99ilyasa@gmail.com>
2020-03-12 13:28:55 +0700

    set basic UI

--- ImageMachinev2/ViewController.swift
+++ ImageMachinev2/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var setTextButton: UIButton!
+    @IBOutlet weak var machineNameLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: Actions
+    @IBAction func setTextButtonPressed(_ sender: UIButton) {
+        machineNameLabel.text = "Default Machine"
+    }
+    
+    
 }
 
